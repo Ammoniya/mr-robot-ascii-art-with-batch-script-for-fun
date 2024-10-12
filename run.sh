fa9e5dde2d816d9b3925f6f872deca1278b8d400
Ammoniya <ravindud@bugzero.io>
2024-10-11 22:23:04 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,45 +2,30 @@
 
 echo "fsociety"
 
-i=0
+while true; do
+    cat art
 
-cat art
 
-# Loop 1
-while [ $i -lt 100 ]; do
-    ((i++))
-    tput setaf 2  # Set text color to green (equivalent to color a)
+    tput setaf 2  # Set text color to green
+    cat art
+
+    sleep 0.2
+
+    tput setaf 1  # Set text color to red
+    cat art
+
+    sleep 0.2
+
+    tput setaf 3  # Set text color to yellow
+    cat art
+
+    sleep 0.2
+
+    tput setaf 7  # Set text color to white
+    cat art
+
+    sleep 0.2
+
 done
 
-# Reset i for next loop
-i=0
-i2=0
-
-# Loop 2
-while [ $i2 -lt 100 ]; do
-    ((i2++))
-    tput setaf 1  # Set text color to red (equivalent to color c)
-done
-
-# Reset i2 for next loop
-i2=0
-i3=0
-
-# Loop 3
-while [ $i3 -lt 100 ]; do
-    ((i3++))
-    tput setaf 3  # Set text color to yellow (equivalent to color d)
-done
-
-# Reset i3 for next loop
-i3=0
-i4=0
-
-# Loop 4
-while [ $i4 -lt 100 ]; do
-    ((i4++))
-    tput setaf 7  # Set text color to white (equivalent to color f)
-done
-
-# Reset terminal to default colors at the end
-tput sgr0
+trap "tput sgr0" EXIT
